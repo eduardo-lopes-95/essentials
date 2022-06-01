fdcce84ecf7c5a709d7a1a72c497392ce5015365
Eduardo Sabino <eduardosabino@ymail.com>
2022-06-01 00:29:53 -0300

    borrow

--- ch7/src/main.rs
+++ ch7/src/main.rs
@@ -1,27 +1,14 @@
-fn main(){
-    // let n = 42;
-    // match n {
-    //     ref r => println!("Got a reference to {}", r),
-    // }
-    
-    // let mut m = 42;
-    // match m {
-    //     ref mut mr => {
-    //         println!("Got a mutable reference to {}", mr);
-    //         *mr = 43;
-    //     }, 
-    // }
-    // println!("m has changed to {}!", m);
-
-    let mag = Magician{name:"Gandalf", power: 4625};
-    let name = {
-        let Magician { name: ref ref_to_name,power: _ } = mag;
-        *ref_to_name
-    };
-    println!("The magician's name is {}", name);
+struct Alien{
+    planet: String,
+    n_tentacles: u32,
 }
-
-struct Magician{
-    name: &'static str,
-    power: i32
+fn main(){
+    let mut klaatu = Alien{planet:"Venus".to_string(),
+        n_tentacles:15 };
+    
+    //let k12 = klaatu; //ownership saiu de klaatu para k12
+    //println!("{}", klaatu.planet); //value borrowed here after move
+    
+    let k12 = &klaatu;
+    println!("{}", klaatu.planet);
 }
