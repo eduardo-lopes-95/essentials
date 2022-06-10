35649c38c5630bb0a06d6e27dbeb4eba89cb4b47
Eduardo Sabino <eduardosabino@ymail.com>
2022-06-09 23:47:48 -0300

    Modulos

--- ch7/src/main.rs
+++ ch7/src/main.rs
@@ -1,33 +1,29 @@
-struct Alien{
-    planet: String,
-    n_tentacles: i32
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Alien {
+    name: String,
+    n_tentacles: u8,
+}
+
+#[derive(Debug)]
+struct Tentacle {
+    poison: u8,
+    owner: Rc<Alien>,
 }
 
 fn main() {
-    /*
-    let mut a1 = Box::new(Alien{planet: "Mars".to_string(), n_tentacles:4});
-    println!("a1 - tentacles: {}", a1.n_tentacles);
-
-    let a2 = &mut a1;
-    println!("a2 - planet: {}", a2.planet);
-    a2.n_tentacles += 1;
-    println!("a2 - tentacles: {}", a2.n_tentacles);
-     */
-    //After such a borrow the usual ownership rules as above hold:
-    // a1 no longer has access, not even for reading
+    let d = Alien {
+        name: "Dharkalen".to_string(),
+        n_tentacles: 7,
+    };
     
-    let n = Box::new(42);
-    
-    /*
-    let p = *n;
-    println!("p: {}", p); //acessando diretamento o conteúdo 42
-    
-    let q = &*n; //(&n) - &Box<i32> = 0xa5f50ffb08, (&*n) - &i32 = 0x279137251a0
-    println!("q: {:p}", q); //acessando a referência 
-     */
-
-     let mut m = n; //n = 42
-     *m = 67; 
-     //println!("{}", n); o valor foi movido
-     println!("{}", m);
+    let d_master = Rc::new(d);
+    for i in 0u8..d_master.n_tentacles {
+        let t = Tentacle {
+            poison: i * 3,
+            owner: d_master.clone(),
+        };
+        println!("{:?}", t);
+    }
 }
